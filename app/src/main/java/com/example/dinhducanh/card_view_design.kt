1b55f23171c549e26aa721eccc88bf6676a7d781
đặng ngọc thư <dangngocthuak@gmail.com>
2022-03-16 14:25:09 +0700

    DinhĐứcAnh_TGK

---
+++ app/src/main/java/com/example/dinhducanh/card_view_design.kt
@@ -0,0 +1,11 @@
+package com.example.dinhducanh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class card_view_design : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.card_view_design)
+    }
+}
